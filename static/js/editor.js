193c2085c9d3ce39740a7686d8bacb03c1cfdfb4
Simon <simon@nimos.ws>
2016-02-09 00:56:06 +0100

    added powerups and player spawn point markers to level editor

--- static/js/editor.js
+++ static/js/editor.js
@@ -43,6 +43,9 @@ var width;
 var height;
 var cursor = [0, 0];
 var rects = [];
+var spawns = [];
+var powerups = [];
+var hist = [];
 var ctx = c.getContext("2d")
 var pt = ctx.createPattern(pattern, "repeat");
 var previewRekt = [0,0,0,0];
@@ -50,6 +53,7 @@ var previewRekt = [0,0,0,0];
 // Draw rects and everything
 var draw = function () {
   ctx.clearRect(0,0,c.width, c.height)
+  drawCursor();
   if (image) ctx.drawImage(image, 2,2);
   ctx.fillStyle = pt;
   for (var i=0; i<rects.length; i++) {
@@ -68,9 +72,21 @@ var draw = function () {
   ctx.fillStyle = fs[bs3.value];
   ctx.fillRect(0,0,2,c.height);
 
-  drawCursor();
+  ctx.fillStyle = "white";
+  for (var i=0; i<spawns.length; i++) {
+    var s = spawns[i];
+    ctx.fillRect(s[0]-5, s[1]-5, 10, 10);
+    ctx.strokeRect(s[0]-5, s[1]-5, 10, 10);
+  }
 
-  generateResult();
+  ctx.fillStyle = "#0f0";
+  for (var i=0; i<powerups.length; i++) {
+    var s = powerups[i];
+    ctx.fillRect(s[0]-10, s[1]-10, 20, 20);
+    ctx.strokeRect(s[0]-10, s[1]-10, 20, 20);
+  }
+
+  drawCursor();
   requestAnimationFrame(draw);
 }
 
@@ -92,21 +108,34 @@ var dragging = -1;
 var start = [0,0];
 c.addEventListener("mousedown", function(e){
   if (e.button != 0) return 1;
+  if (tool.value == "c") {
     start = [e.offsetX, e.offsetY];
     dragging = 0;
+    hist.push("c");
+  } else if (tool.value == "s") {
+    hist.push("s");
+    spawns.push([e.offsetX, e.offsetY]);
+  } else if (tool.value == "p") {
+    hist.push("p");
+    powerups.push([e.offsetX, e.offsetY]);
+  }
+  generateResult();
 }, false);
 c.addEventListener("mousemove", function(e){
     cursor = [e.offsetX, e.offsetY];
-    if (dragging != -1) {
-      dragging = 1;
-    } else {
-      return;
+    if (tool.value == "c") {
+      if (dragging != -1) {
+        dragging = 1;
+      } else {
+        return;
+      }
+      var end = [e.offsetX, e.offsetY];
+      var rectsize = [end[0]-start[0], end[1]-start[1]]
+      previewRekt = [start[0], start[1], rectsize[0], rectsize[1]]
     }
-    var end = [e.offsetX, e.offsetY];
-    var rectsize = [end[0]-start[0], end[1]-start[1]]
-    previewRekt = [start[0], start[1], rectsize[0], rectsize[1]]
 }, false);
 c.addEventListener("mouseup", function(e){
+  if (tool.value == "c") {
     var end = [e.offsetX, e.offsetY];
     var rectsize = [end[0]-start[0], end[1]-start[1]]
     previewRekt = [0,0,0,0];
@@ -115,9 +144,17 @@ c.addEventListener("mouseup", function(e){
         generateResult();
     }
     dragging= -1;
+  }
 }, false);
 c.addEventListener("contextmenu", function (e) {
-  rects.pop();
+  var t = hist.pop();
+  if (t=="c") {
+    rects.pop();
+  } else if (t=="s") {
+    spawns.pop();
+  } else if (t=="p") {
+    powerups.pop();
+  }
   generateResult();
   e.preventDefault();
   return false;
@@ -137,7 +174,6 @@ var generateResult = function () {
       r[0]+=r[2];
       r[2]= r[2]*-1;
     }
-    console.log(r[3])
     if (r[3] < 0) {
       r[1] += r[3];
       r[3] = r[3]*-1;
@@ -147,11 +183,93 @@ var generateResult = function () {
   result = result.slice(0,-1);
   result += "\n\t],\n";
   result += "\t\"backgroundImage\": \"\",\n"
-  result += "\t\"foregroundImage\": \"\"\n"
+  result += "\t\"foregroundImage\": \"\",\n"
   result += "\t\"bounds\": ["+bs0.value+","+bs1.value+","+bs2.value+","+bs3.value+"],\n"
   result += "}"
 
   document.getElementById('result').value = result;
 }
+
+var generateResult = function () {
+  var result = {}
+  result["size"] = [width, height];
+  result["rects"] =[]
+  for (var i=0; i<rects.length; i++) {
+    var r = [rects[i][0], rects[i][1], rects[i][2], rects[i][3]];
+    r[1] = height-r[1];
+    r[3] *= -1;
+    if (r[2] < 0) {
+      r[0]+=r[2];
+      r[2]= r[2]*-1;
+    }
+    if (r[3] < 0) {
+      r[1] += r[3];
+      r[3] = r[3]*-1;
+    }
+    result["rects"].push(r);
+  }
+  console.log(result);
+
+  result["powerups"] = []
+  for (var i=0; i<powerups.length; i++) {
+    var r = [powerups[i][0], powerups[i][1]];
+    r[1] = height-r[1];
+    result["powerups"].push(r);
+  }
+
+  result["spawns"] = []
+  console.log(spawns)
+  for (var i=0; i<spawns.length; i++) {
+    var r = [spawns[i][0], spawns[i][1]];
+    r[1] = height-r[1];
+    console.log(r);
+    result["spawns"].push(r);
+  }
+
+  result["backgroundImage"] = "";
+  result["foregroundImage"] = "";
+  result["bounds"] = [bs0.value, bs1.value, bs2.value, bs3.value];
+  document.getElementById('result').value = JSON.stringify(result, null, 3);
+}
+
+document.getElementById('result').oninput = function (e) {
+  var m = eval("m = "+this.value)
+  width = m.size[0];
+  height = m.size[1];
+  c.width = width+2;
+  c.height = height+2;
+  bs0.value = m.bounds[0];
+  bs1.value = m.bounds[1];
+  bs2.value = m.bounds[2];
+  bs3.value = m.bounds[3];
+  rects = [];
+  for (var i=0; i<m.rects.length; i++) {
+    r=m.rects[i];
+    r[1] = height-r[1];
+    r[3] *= -1;
+    rects.push(r);
+  }
+  powerups = []
+  for (var i=0; i<m.powerups.length; i++) {
+    var r=m.powerups[i];
+    r[1] = height-r[1];
+    powerups.push(r);
+  }
+  spawns = []
+  for (var i=0; i<m.spawns.length; i++) {
+    var r=m.spawns[i];
+    r[1] = height-r[1];
+    spawns.push(r);
+  }
+
+  generateResult();
+}
+
+bs0.onchange = generateResult;
+bs1.onchange = generateResult;
+bs2.onchange = generateResult;
+bs3.onchange = generateResult;
+
+
 generateResult();
 draw();
